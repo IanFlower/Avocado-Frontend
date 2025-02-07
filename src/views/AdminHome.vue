95143aee3203a84f66d943db002adb595d90262e
IanBirdcatcher <ian.fowler@eagles.oc.edu>
2025-02-07 12:59:30 -0600

    Init commit

--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -4,20 +4,93 @@ import userImg from "../assets/User.svg";
 
 <template>
     <v-container>
-        <v-row>
-            <v-col><v-card class="" height="100" width="75%">
-                <svg width="800px" height="800px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="309.912" cy="82.088" r="34.088" fill="var(--ci-primary-color, #000000)" class="ci-primary"/>
-                    <path fill="var(--ci-primary-color, #000000)" d="M322,143.462l-60.585-20.64L183.8,151.048A24.073,24.073,0,0,0,168,173.6V232h32V179.207l48.811-17.749L158.622,440h33.613l50.082-155.811,7.871,2.568L288,356.079V440h32V354.04a24.068,24.068,0,0,0-2.931-11.493l-37.56-68.861,28.949-88.715,19.27,34.684A24.011,24.011,0,0,0,348.707,232H400V200H353.414Z" class="ci-primary"/>
-                </svg>
-                <v-card-title>Users</v-card-title>
-                <v-card-subtitle><v-divider vertical thickness="20px"></v-divider> Manage Student Worker Permissions/Admins/Professors</v-card-subtitle>
-            </v-card></v-col>
-            <v-col></v-col>
+        <v-row class="pb-15">
+            <v-col>
+                <v-card width="480px" link>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="1">
+                            <svg width="55px" height="55px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="309.912" cy="82.088" r="34.088" fill="var(--ci-primary-color, #000000)" class="ci-primary"/>
+                                <path fill="var(--ci-primary-color, #000000)" d="M322,143.462l-60.585-20.64L183.8,151.048A24.073,24.073,0,0,0,168,173.6V232h32V179.207l48.811-17.749L158.622,440h33.613l50.082-155.811,7.871,2.568L288,356.079V440h32V354.04a24.068,24.068,0,0,0-2.931-11.493l-37.56-68.861,28.949-88.715,19.27,34.684A24.011,24.011,0,0,0,348.707,232H400V200H353.414Z" class="ci-primary"/>
+                            </svg>
+                            </v-col>
+                            <v-col>
+                            <v-card-title>Users</v-card-title>
+                            <v-card-subtitle>
+                                <v-divider vertical thickness="20px"></v-divider>
+                                Manage Student Worker Permissions/Admins/Professors
+                            </v-card-subtitle>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card width="480px" link>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="1">
+                            <svg width="55px" height="55px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="309.912" cy="82.088" r="34.088" fill="var(--ci-primary-color, #000000)" class="ci-primary"/>
+                                <path fill="var(--ci-primary-color, #000000)" d="M322,143.462l-60.585-20.64L183.8,151.048A24.073,24.073,0,0,0,168,173.6V232h32V179.207l48.811-17.749L158.622,440h33.613l50.082-155.811,7.871,2.568L288,356.079V440h32V354.04a24.068,24.068,0,0,0-2.931-11.493l-37.56-68.861,28.949-88.715,19.27,34.684A24.011,24.011,0,0,0,348.707,232H400V200H353.414Z" class="ci-primary"/>
+                            </svg>
+                            </v-col>
+                            <v-col>
+                            <v-card-title>Users</v-card-title>
+                            <v-card-subtitle>
+                                <v-divider vertical thickness="20px"></v-divider>
+                                Manage Student Worker Permissions/Admins/Professors
+                            </v-card-subtitle>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
         </v-row>
-        <v-row>
-            <v-col></v-col>
-            <v-col></v-col>
+        <v-row class="pb-15">
+            <v-col>
+                <v-card width="480px" link>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="1">
+                            <svg width="55px" height="55px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="309.912" cy="82.088" r="34.088" fill="var(--ci-primary-color, #000000)" class="ci-primary"/>
+                                <path fill="var(--ci-primary-color, #000000)" d="M322,143.462l-60.585-20.64L183.8,151.048A24.073,24.073,0,0,0,168,173.6V232h32V179.207l48.811-17.749L158.622,440h33.613l50.082-155.811,7.871,2.568L288,356.079V440h32V354.04a24.068,24.068,0,0,0-2.931-11.493l-37.56-68.861,28.949-88.715,19.27,34.684A24.011,24.011,0,0,0,348.707,232H400V200H353.414Z" class="ci-primary"/>
+                            </svg>
+                            </v-col>
+                            <v-col>
+                            <v-card-title>Users</v-card-title>
+                            <v-card-subtitle>
+                                <v-divider vertical thickness="20px"></v-divider>
+                                Manage Student Worker Permissions/Admins/Professors
+                            </v-card-subtitle>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card width="480px" link>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="1">
+                            <svg width="55px" height="55px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="309.912" cy="82.088" r="34.088" fill="var(--ci-primary-color, #000000)" class="ci-primary"/>
+                                <path fill="var(--ci-primary-color, #000000)" d="M322,143.462l-60.585-20.64L183.8,151.048A24.073,24.073,0,0,0,168,173.6V232h32V179.207l48.811-17.749L158.622,440h33.613l50.082-155.811,7.871,2.568L288,356.079V440h32V354.04a24.068,24.068,0,0,0-2.931-11.493l-37.56-68.861,28.949-88.715,19.27,34.684A24.011,24.011,0,0,0,348.707,232H400V200H353.414Z" class="ci-primary"/>
+                            </svg>
+                            </v-col>
+                            <v-col>
+                            <v-card-title>Users</v-card-title>
+                            <v-card-subtitle>
+                                <v-divider vertical thickness="20px"></v-divider>
+                                Manage Student Worker Permissions/Admins/Professors
+                            </v-card-subtitle>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
         </v-row>
     </v-container>
 </template>

ae19206ffb67630f83e0d394e85dc6bbd7c09a2f
IanBirdcatcher <ian.fowler@eagles.oc.edu>
2025-02-08 11:07:25 -0600

    Added styling and menubar compatibility

--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -6,7 +6,7 @@ import userImg from "../assets/User.svg";
     <v-container>
         <v-row class="pb-15">
             <v-col>
-                <v-card width="480px" link>
+                <v-card class="secondary" width="480px" link>
                     <v-container>
                         <v-row>
                             <v-col cols="1">
@@ -27,7 +27,54 @@ import userImg from "../assets/User.svg";
                 </v-card>
             </v-col>
             <v-col>
-                <v-card width="480px" link>
+                <v-card class="secondary" width="480px" link>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="1">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50px" height="50px" viewBox="0 0 24 24" version="1.1">
+                                    <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+                                    <title>ic_fluent_checkbox_checked_24_regular</title>
+                                    <desc>Created with Sketch.</desc>
+                                    <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <g id="ic_fluent_checkbox_checked_24_regular" fill="#212121" fill-rule="nonzero">
+                                            <path d="M18.25,3 C19.7687831,3 21,4.23121694 21,5.75 L21,18.25 C21,19.7687831 19.7687831,21 18.25,21 L5.75,21 C4.23121694,21 3,19.7687831 3,18.25 L3,5.75 C3,4.23121694 4.23121694,3 5.75,3 L18.25,3 Z M18.25,4.5 L5.75,4.5 C5.05964406,4.5 4.5,5.05964406 4.5,5.75 L4.5,18.25 C4.5,18.9403559 5.05964406,19.5 5.75,19.5 L18.25,19.5 C18.9403559,19.5 19.5,18.9403559 19.5,18.25 L19.5,5.75 C19.5,5.05964406 18.9403559,4.5 18.25,4.5 Z M10,14.4393398 L16.4696699,7.96966991 C16.7625631,7.6767767 17.2374369,7.6767767 17.5303301,7.96966991 C17.7965966,8.23593648 17.8208027,8.65260016 17.6029482,8.94621165 L17.5303301,9.03033009 L10.5303301,16.0303301 C10.2640635,16.2965966 9.84739984,16.3208027 9.55378835,16.1029482 L9.46966991,16.0303301 L6.46966991,13.0303301 C6.1767767,12.7374369 6.1767767,12.2625631 6.46966991,11.9696699 C6.73593648,11.7034034 7.15260016,11.6791973 7.44621165,11.8970518 L7.53033009,11.9696699 L10,14.4393398 L16.4696699,7.96966991 L10,14.4393398 Z" id="🎨Color"></path>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </v-col>
+                            <v-col>
+                            <v-card-title>Users</v-card-title>
+                            <v-card-subtitle>
+                                <v-divider vertical thickness="20px"></v-divider>
+                                Manage Student Worker Permissions/Admins/Professors
+                            </v-card-subtitle>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row class="pb-15">
+            <v-col>
+                <v-card class="secondary" width="480px" link>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="1">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2.5 4.25C2.5 3.83579 2.83579 3.5 3.25 3.5H3.80826C4.75873 3.5 5.32782 4.13899 5.65325 4.73299C5.87016 5.12894 6.02708 5.58818 6.14982 6.00395C6.18306 6.00134 6.21674 6 6.2508 6H18.7481C19.5783 6 20.1778 6.79442 19.9502 7.5928L18.1224 14.0019C17.7856 15.1832 16.7062 15.9978 15.4779 15.9978H9.52977C8.29128 15.9978 7.2056 15.1699 6.87783 13.9756L6.11734 11.2045L4.85874 6.95578L4.8567 6.94834C4.701 6.38051 4.55487 5.85005 4.33773 5.4537C4.12686 5.0688 3.95877 5 3.80826 5H3.25C2.83579 5 2.5 4.66421 2.5 4.25ZM7.57283 10.8403L8.32434 13.5786C8.47333 14.1215 8.96682 14.4978 9.52977 14.4978H15.4779C16.0362 14.4978 16.5268 14.1275 16.68 13.5906L18.4168 7.5H6.58549L7.55906 10.7868C7.56434 10.8046 7.56892 10.8224 7.57283 10.8403Z" fill="#212121"></path> <path d="M11 19C11 20.1046 10.1046 21 9 21C7.89543 21 7 20.1046 7 19C7 17.8954 7.89543 17 9 17C10.1046 17 11 17.8954 11 19ZM9.5 19C9.5 18.7239 9.27614 18.5 9 18.5C8.72386 18.5 8.5 18.7239 8.5 19C8.5 19.2761 8.72386 19.5 9 19.5C9.27614 19.5 9.5 19.2761 9.5 19Z" fill="#212121"></path> <path d="M18 19C18 20.1046 17.1046 21 16 21C14.8954 21 14 20.1046 14 19C14 17.8954 14.8954 17 16 17C17.1046 17 18 17.8954 18 19ZM16.5 19C16.5 18.7239 16.2761 18.5 16 18.5C15.7239 18.5 15.5 18.7239 15.5 19C15.5 19.2761 15.7239 19.5 16 19.5C16.2761 19.5 16.5 19.2761 16.5 19Z" fill="#212121"></path> </g></svg>
+                            </v-col>
+                            <v-col>
+                            <v-card-title>Users</v-card-title>
+                            <v-card-subtitle>
+                                <v-divider vertical thickness="20px"></v-divider>
+                                Manage Student Worker Permissions/Admins/Professors
+                            </v-card-subtitle>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card class="secondary" width="480px" link>
                     <v-container>
                         <v-row>
                             <v-col cols="1">
@@ -48,9 +95,9 @@ import userImg from "../assets/User.svg";
                 </v-card>
             </v-col>
         </v-row>
-        <v-row class="pb-15">
+        <v-row>
             <v-col>
-                <v-card width="480px" link>
+                <v-card class="secondary" width="480px" link>
                     <v-container>
                         <v-row>
                             <v-col cols="1">
@@ -71,7 +118,7 @@ import userImg from "../assets/User.svg";
                 </v-card>
             </v-col>
             <v-col>
-                <v-card width="480px" link>
+                <v-card class="secondary" width="480px" link>
                     <v-container>
                         <v-row>
                             <v-col cols="1">

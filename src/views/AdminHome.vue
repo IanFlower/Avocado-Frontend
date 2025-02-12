65aa8572e4bb68873aec27c7cb4f1b35057d136e
IanBirdcatcher <ian.fowler@eagles.oc.edu>
2025-02-12 13:27:47 -0600

    Init commit

---
+++ src/views/AdminHome.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+
+<v-data-table
+    v-model:expanded="expanded"
+    :headers="dessertHeaders"
+    :items="desserts"
+    item-value="name"
+    show-expand
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Expandable Table</v-toolbar-title>
+      </v-toolbar>
+    </template>
+    <template v-slot:expanded-row="{ columns, item }">
+      <tr>
+        <td :colspan="columns.length">
+          More info about {{ item.name }}
+        </td>
+      </tr>
+    </template>
+  </v-data-table>
+    
+</template>

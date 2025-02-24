a828e6eb1210fae566f74416b27896f855891b3f
Sage Myers <sage.myers@eagles.oc.edu>
2025-02-24 14:04:51 -0600

    made stuff for the leaderboard

--- src/views/leaderBoard.vue
+++ src/views/leaderBoard.vue
@@ -1,24 +1,38 @@
 <template>
-    
-<v-card class="d-flex flex-column pa-4 text-center primary" height="400px" width="70%" elevation="12">
-    <v-card-title class="text-subtitle-1">Leaderboard</v-card-title>
-    <v-divider></v-divider>
-
-    <!-- Leaderboard buttons -->
-    <v-btn v-for="n in 5" :key="n" class="mb-2" @click="goHome">
-      Leaderboard Placeholder {{ n }}
-    </v-btn>
-  </v-card>
+  <v-container fluid class="fill-height">
+    <v-row>
+      <v-col cols="12" style="height: 100%" md="1">
+        <!-- Sidebar content goes here -->
+        <v-card style="height: 100%" class = "primary">
+          <v-card-title>Leaderboard</v-card-title>
+          <v-tabs v-model="tab" direction="vertical"> 
+            <v-tab>Top 8</v-tab>
+            <v-tab>Recent</v-tab>
+            <v-tab>All Time</v-tab>
+          </v-tabs>
+        </v-card>
+      </v-col>
+      <v-col>
+        <!-- Main content goes here -->
+        <v-card height="100%">
+          <v-card-text>
+            <!-- Add your main content here -->
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-
-  const goHome = () => {
-    router.push('/home'); 
-  };
-
+const tab = ref(0)
 </script>
+
+<style scoped>
+.v-card {
+  height: 100%;
+}
+</style>
+

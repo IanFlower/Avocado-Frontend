6d0091a8c1e253170b521a77bbc29ade0828562b
SypnoticVoid <logan.murrie@eagles.oc.edu>
2025-03-26 14:03:25 -0500

    icon schtuff

--- src/services/iconServices.js
+++ src/services/iconServices.js
@@ -1,51 +1,32 @@
-import Axios from 'axios';
+// iconService.js
 import apiClient from '../services/services';
 
-const iconService = {
+const IconService = {
+  // Get all icons
   getAllIcons() {
     return apiClient.get('/icon');
   },
 
+  // Get a specific icon by ID
   getIconById(iconId) {
     return apiClient.get(`/icon/${iconId}`);
   },
 
-  addIcon(icon) {
-    const formData = new FormData();
-
-    
-    if (!icon.image || !(icon.image instanceof File)) {
-      console.error("Invalid file object:", icon.image);
-      throw new Error("Invalid file object");
-    }
-
-    formData.append('image', icon.image);
-    formData.append('forBadge', icon.forBadge);
-
-    console.log("FormData before sending:", formData);
-
-    return Axios.post('http://localhost:3032/flight-plan-t2/icon/upload', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        console.error("Error during file upload:", error);
-        throw error;
-      });
+  //creates a new icon
+  addIcon(icon)
+  {
+    return apiClient.post(`/icon/`, icon);
   },
-
 
   updateIcon(icon) {
-    return apiClient.put(`/icon/${icon.id}`, icon);
+    return apiClient.put(`/icon/${icon.id}`, icon); 
   },
+  
 
-  DeleteIcon(iconId) {
+  DeleteIcon(iconId)
+  {
     return apiClient.delete(`/icon/${iconId}`);
   }
 };
 
-export default iconService;
+export default IconService;

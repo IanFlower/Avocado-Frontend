b0d7d7b8851c1c7a0dadec0fc81b05ad5e0793c2
SypnoticVoid <logan.murrie@eagles.oc.edu>
2025-04-16 14:22:24 -0500

    fixed route for the shop page so that it will show the correct points and route back to the home page CORRECTLY

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -5,11 +5,13 @@ import iconServices from '../services/iconServices.js';
 import rewardServices from '../services/rewardServices.js';
 import { useRouter } from 'vue-router';
 // Helper Refs
+
+const router = useRouter();
 const search = ref('');
 const modal = ref(false);
 const selectedReward = ref(null);
 // Data Refs
-const rewardsList = computed(() => rewards.value.filter(reward => { if(search.value == '') return true; return reward.name.toLowerCase().includes(search.value.toLowerCase())}));
+const rewardsList = computed(() => rewards.value.filter(reward => { if (search.value == '') return true; return reward.name.toLowerCase().includes(search.value.toLowerCase()) }));
 const rewards = ref([]);
 
 onMounted(async () => {
@@ -20,7 +22,7 @@ onMounted(async () => {
       reward.image_file = null;
       if (reward.image) {
         iconServices.getIconByFile(reward.image).then(icon => {
-          if(icon.data){
+          if (icon.data) {
             reward.image_file = icon.data;
           }
         });
@@ -31,6 +33,10 @@ onMounted(async () => {
   }
 });
 
+const goToStudentHome = () => {
+  router.push({ name: 'StudentHome' });
+};
+
 </script>
 
 <template>
@@ -39,8 +45,8 @@ onMounted(async () => {
       <v-col cols="6" offset-md="3" align="center">
         <h1>Student Shop</h1>
       </v-col>
-      <v-col cols="3" class="d-flex justify-center">
-        <v-btn class="text-center disable-events accent clickable-card py-8 px-13 d-flex align-center justify-center"
+      <v-col align="center">
+        <v-btn class="text-center accent clickable-card py-8 px-13 d-flex align-center justify-center" @click="goToStudentHome"
           elevation="6" size="x-large">
           <div class="d-flex flex-column align-center">
             <span class="font-weight-bold">Points:</span>
@@ -60,15 +66,17 @@ onMounted(async () => {
         <v-row>
           <v-col cols="3" v-for="reward in rewardsList" :key="reward">
             <v-card height="35vh" width="15vw" class="secondary" @click="selectedReward = reward, modal = true">
-              <v-img :src="reward.image_file ? `data:image/*;base64,${reward.image_file}` : NoImageAvailable"  cover crop height="28vh" width="15vw"></v-img>
+              <v-img :src="reward.image_file ? `data:image/*;base64,${reward.image_file}` : NoImageAvailable" cover crop
+                height="28vh" width="15vw"></v-img>
               <v-spacer></v-spacer>
-              <v-card-actions >
+              <v-card-actions>
                 <v-row height="7vh" class="align-center mb-1 mx-auto">
                   <v-col cols="8">
                     <span class="text-h6">{{ reward.name }}</span>
                   </v-col>
                   <v-col cols="4">
-                    <v-btn :icon="`mdi-numeric-${reward.requiredPoints}`" width="2.8vw" height="2.8vw" class="accent-button disable-events">
+                    <v-btn :icon="`mdi-numeric-${reward.requiredPoints}`" width="2.8vw" height="2.8vw"
+                      class="accent-button disable-events">
                       {{ reward.requiredPoints }}
                     </v-btn>
                   </v-col>
@@ -79,10 +87,10 @@ onMounted(async () => {
         </v-row>
       </v-col>
     </v-row>
-    <v-dialog v-model="modal" width="30vw" max-height="30vh" >
+    <v-dialog v-model="modal" width="30vw" max-height="30vh">
       <v-card>
         <v-card-text>
-          Description:  {{ selectedReward.desc }}
+          Description: {{ selectedReward.desc }}
         </v-card-text>
         <v-divider></v-divider>
         <v-card-actions>
